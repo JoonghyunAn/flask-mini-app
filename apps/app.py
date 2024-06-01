38e13a41fd8aad00ca74d12e7a470ab0739e7817
JoonghyunAn <anjoonghyun92@gmail.com>
2024-06-01 14:42:06 +0900

    SQLAlchemy_SQLite setup

--- apps/app.py
+++ apps/app.py
@@ -1,8 +1,24 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# making SQLAlchemy instance
+db = SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_mapping(
+        SECRET_KEY = "asdflkjqwioelkj1235sdf",
+        SQLALCHEMY_DATABASE_URI=
+        f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+    # SQLAlalchemy & app 
+    db.init_app(app)
+    # Migrate & app
+    Migrate(app,db)
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
