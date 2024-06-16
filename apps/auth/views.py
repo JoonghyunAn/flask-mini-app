d64fea206e91b3e1540b121650dcf17ad75c120b
JoonghyunAn <anjoonghyun92@gmail.com>
2024-06-16 12:15:13 +0900

    detector_signup_updated

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -44,7 +44,7 @@ def login():
 
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
-            return redirect(url_for("crud.users"))
+            return redirect(url_for("detector.index"))
         
         flash("Invalid email or password")
     return render_template("auth/login.html",form=form)
--- apps/detector/views.py
+++ apps/detector/views.py
@@ -11,6 +11,6 @@ def index():
         db.session.query(User,UserImage)
         .join(UserImage)
         .filter(User.id == UserImage.user_id)
-        .all
+        .all()
     )
     return render_template("detector/index.html", user_images=user_images)
